2b761c29838c61fa861fb072572616046bfb30d2
Paul <mets1005@yahoo.com>
2022-10-26 15:26:51 -0400

    finished FK with unit lengths, need to find actual joint distances

--- ForwardKinematics.py
+++ ForwardKinematics.py
@@ -35,21 +35,27 @@ def rotx(q):
     # Convert joint angle q from degrees -> radians
     q = q * math.pi / 180
     R = [[1, 0, 0], [0, math.cos(q), -1*math.sin(q)], [0, math.sin(q), math.cos(q)]]
-    return R
+    R = np.array(R)
+    #print(str(R.reshape(3,3)) + "\n")
+    return R.reshape(3,3)
 
 # Define the rotation about the y function for given joint angle q
 def roty(q):
     # Convert joint angle q from degrees -> radians
     q = q * math.pi / 180
     R = [[math.cos(q), 0, math.sin(q)], [0, 1, 0], [-1*math.sin(q), 0, math.cos(q)]]
-    return R
+    R = np.array(R)
+    #print(str(R.reshape(3,3)) + "\n")
+    return R.reshape(3,3)
 
 # Define the rotation about the z function for given joint angle q
 def rotz(q):
     # Convert joint angle q from degrees -> radians
     q = q * math.pi / 180
     R = [[math.cos(q), -1*math.sin(q), 0], [math.sin(q), math.cos(q), 0], [0, 0, 1]]
-    return R
+    R = np.array(R)
+    #print(str(R.reshape(3,3)) + "\n")
+    return R.reshape(3,3)
 
 # Define the 3x3 identity matrix
 def identity():
@@ -59,7 +65,6 @@ def identity():
 # User input for joint angles
 def angles():
     q = [0, 0, 0, 0, 0, 0]
-    print("INPUT ROTATION ANGLES FOR ALL 3 SERVOS")
     for i in range (0,6):
         print("\nEnter angle for joint " + str(i+1) + " in degrees [-90,90]")
         q[i] = input()
@@ -78,11 +83,28 @@ def angles():
                 print("Enter angle for joint " + str(i+1) + " in degrees [-90,90]")
                 q[i] = input()
 
-    print("\nYour joint angles are:")
+    print("\nYour joint angles are: [deg]\n")
     for i in range (0,6):
         print("\tJoint " + str(i+1) + ": " + str(q[i]))
     return q
 
+# Form the homogenous transform with R0T and P0T
+def homogTransf(R0T, P0T):
+    H = np.zeros((len(R0T)+1, len(R0T)+1))
+    for i in range(0, len(R0T)+1):
+        for j in range(0, len(R0T[0])+1):
+            if i < len(R0T) and j < len(R0T[0]):
+                H[i][j] = R0T[i][j]
+            elif j < len(R0T[0]):
+                H[i][j] = 0
+            elif i < len(R0T):
+                H[i][j] = P0T[i][0]
+            else :
+                H[i][j] = 1
+    
+    H = np.array(H)
+    H = H.reshape(len(R0T)+1, len(R0T)+1)
+    return H
 
 # Main function for program
 def main():
@@ -90,6 +112,8 @@ def main():
     # Clear console screen
     clear = lambda: os.system('clear')
     clear()
+    print("------------")
+    print("FORWARD KINEMATICS")
 
     # Define the rotation matrices from i-1 -> i
     q = angles()
@@ -101,15 +125,16 @@ def main():
     q6 = q[5]
 
     # Define all i-1 -> i rotations
-    R01 = rotz(q1)
-    R12 = roty(q2)
-    R23 = roty(q3)
-    R34 = roty(q4)
-    R45 = rotz(q5)
-    R56 = rotx(q6)
-    R6T = identity()
+    R01 = np.array(rotz(q1))
+    R12 = np.array(roty(q2))
+    R23 = np.array(roty(q3))
+    R34 = np.array(roty(q4))
+    R45 = np.array(rotz(q5))
+    R56 = np.array(rotx(q6))
+    R6T = np.array(identity())
 
     # Define all the joint lengths
+    # FIND THE ACTUAL JOINT LENGTHS
     l0 = 1
     l1 = 1
     l2 = 1
@@ -120,14 +145,55 @@ def main():
 
     # Define the position vectors from i-1 -> i
     P01 = (l0 + l1) * ez()
-    P12 = 0
+    P12 = 0 * ey()
     P23 = l2 * ez()
     P34 = l3 * ez()
     P45 = (l4 + l5 + l6) * ez()
-    P56 = 0
-    P6T = 0
+    P56 = 0 * ez()
+    P6T = 0 * ex()
 
-    print("\nSUCCESS")
+    # Final rotation matrix 0 -> T
+    R02 = np.dot(R01,R12)
+    R03 = np.dot(R02,R23)
+    R04 = np.dot(R03,R34)
+    R05 = np.dot(R04,R45)
+    R06 = np.dot(R05,R56)
+    R0T = np.dot(R06,R6T)
+    
+
+    # Final position vector 0 -> T
+    P0T = P01 + np.dot(R01,P12) + np.dot(R02,P23) + np.dot(R03,P34) + np.dot(R04,P45) + np.dot(R05,P56) + np.dot(R06,P6T)
+
+    # Print final rotation
+    print("\nThe final rotation R0T is:\n")# + str(R0T.reshape(3,3)))
+    # Round R0T to 3 decimal places
+    for i in range(0, len(R0T)):
+        for j in range(0, len(R0T[i])):
+            if R0T[i][j] == -0:
+                R0T[i][j] = 0
+            print("\t" + str(format(R0T[i][j], '.3f')), end = "\t")
+        print()
+
+    # Print final position
+    print("\nThe final position P0T is: [mm]\n")# + str(P0T.reshape(3,1)))
+    # Round P0T to 3 decimal places
+    for i in range(0, len(P0T)):
+        if P0T[i] == -0:
+            P0T[i] = 0
+        print("\t" + str(format(P0T[i][0], '.3f')))
+
+    # Print homogenous transform H0T
+    print("\nThe homenous transform H0T is:\n")
+    H = homogTransf(R0T, P0T)
+    # Round H0T to 3 decimal places
+    for i in range(0, len(H)):
+        for j in range(0, len(H[i])):
+            print("\t" + str(format(H[i][j], '.3f')), end = "\t")
+        print()
+
+
+    # End of Code
+    print("\n------------")
 
 if __name__ == "__main__" :
     main()
---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array([[1,0,0],[0,1,0],[0,0,1]])
+v1 = np.array([[0],[0],[0]])
+
+print(A)
+print()
+print(v1)
+print()
+print(np.dot(A,v1))
+
+print()
+print(str(format(5.123456789, '.6f')))
